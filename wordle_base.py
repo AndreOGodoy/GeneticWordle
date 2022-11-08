c79c2e66e53a1854e04cdd825847b2e27b667c9f
caioguedesam <caioguedesam@gmail.com>
2022-11-08 16:50:17 -0300

    implementing green letter "probability"

--- wordle_base.py
+++ wordle_base.py
@@ -1,6 +1,31 @@
 import random
 import gen_instance
 
+def green_letter_distribution(word_list, word_len = 5):
+    dist_probability = {}
+    word_count = len(word_list) 
+    for i in range(word_count):
+        word = word_list[i]
+        for j in range(len(word)):
+            if word[j] in dist_probability:
+                dist_probability[word[j]][j] += 1
+            else:
+                dist_probability[word[j]] = [0 for x in range(word_len)]
+    
+    for letter in dist_probability:
+        for i in range(len(dist_probability[letter])):
+            dist_probability[letter][i] /= word_count
+
+    return dist_probability
+
+def green_letter_probability(word, gld):
+    result = 0
+
+    for i in range(len(word)):
+        result += gld[word[i]][i]
+
+    return result
+
 def evaluate_guess(guess, secret):
     result = []
     for i in range(len(guess)):
@@ -50,7 +75,16 @@ def solve_wordle(words, secret, max_guesses = 50):
     # Oops, exceeded number of max guesses (this shouldn't happen with heuristics, I think)
     return False
 
+def print_glp(word, gld):
+    glp = green_letter_probability(word, gld)
+    print(f'The green letter probability for {word} is {glp}.')
+
+
 if __name__ == '__main__':
     word_list, secret = gen_instance.generate_from_wordle_list()
-    result = solve_wordle(word_list, secret)
-    print(result)
+    # result = solve_wordle(word_list, secret)
+    # print(result)
+    gld = green_letter_distribution(word_list)
+    
+    print_glp('ANDRE', gld)
+    print_glp('JULIA', gld)
